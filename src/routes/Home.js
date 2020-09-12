1866763132e5530c985f9e771fbcc65bb8e719cc
chobobdev <chobobdev@naver.com>
2020-09-12 22:57:10 +0900

    #3.0 Form and Databse Setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [bweet,setBweet] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault();
+    };
+    const onChange = (event)=> {
+        const {
+            target: {value}
+        } = event;
+        setBweet(value);
+
+    };
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={bweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}/>
+                <input type="submit" value="Bweet" />
+            </form>
+        </div>
+    );
+}
 export default Home;
