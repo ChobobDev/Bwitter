792a7af7dce926c3221b204d16274bff444aaa51
chobobdev <chobobdev@naver.com>
2020-09-13 00:15:55 +0900

    #3.1 Nweeting!

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -2,81 +2,88 @@ import React, { useState } from "react";
 import { authService, firebaseInstance } from "huobase";
 
 const Auth = () => {
-    const [email,setEmail] = useState(""); //이메일에 관한 State
-    const [password,setPassword] = useState(""); //비밀번호에 관한 State
-    const [newAccount,setNewAccount] = useState(true);
-    const [error, setError] = useState("");
-    const onChange = (event) =>{
-        const {target: {name,value}}=event;
-        if(name === "email"){
-            setEmail(value)
-        }else if(name==="password"){
-            setPassword(value);
-        }
-    };
-    const onSubmit = async(event) => {
-        event.preventDefault();
-        try{
-            if (newAccount) {
-                await authService.createUserWithEmailAndPassword(email,password);
-            } else {
-                await authService.signInWithEmailAndPassword(email,password);
-            }
-        }catch (error){
-            setError(error.message);
-        }
-        
-    };
-
-    const toggleAccount = () => setNewAccount((prev) => !prev);
-    const onSocialClick = async (event) =>{
-        //es6
-        const {
-            target:{name},
-        } = event;
-        let provider;
-        if(name=== "google"){
-            provider = new firebaseInstance.auth.GoogleAuthProvider();
-        }else if(name==="github"){
-            provider = new firebaseInstance.auth.GithubAuthProvider();
-        }
-        await authService.signInWithPopup(provider);
-        
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
     }
-
-
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input 
-                name="email" 
-                type="text" 
-                placeholder="Email" 
-                required 
-                value={email} 
-                onChange={onChange}
-                />
-                
-
-                <input 
-                name="password" 
-                type="password"
-                placeholder="Password" 
-                required
-                value={password} 
-                onChange={onChange}
-                />
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
-                {error}
-            </form>
-
-    <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
-
-            <div>
-                <button onClick={onSocialClick} name="google"> Continue with Google</button>
-                <button onClick={onSocialClick} name="github"> Continue with Github</button>
-            </div>
-        </div>
-    );
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  const toggleAccount = () => setNewAccount((prev) => !prev);
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign In"}
+        />
+        {error}
+      </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign In" : "Create Account"}
+      </span>
+      <div>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
+      </div>
+    </div>
+  );
 };
 export default Auth;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,24 +1,35 @@
 import React, { useState } from "react";
+import { dbService } from "huobase";
 
 const Home = () => {
-    const [bweet,setBweet] = useState("");
-    const onSubmit = (event) => {
-        event.preventDefault();
-    };
-    const onChange = (event)=> {
-        const {
-            target: {value}
-        } = event;
-        setBweet(value);
-
-    };
-    return(
-        <div>
-            <form onSubmit={onSubmit}>
-                <input value={bweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}/>
-                <input type="submit" value="Bweet" />
-            </form>
-        </div>
-    );
-}
+  const [bweet, setBweet] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("bweets").add({
+      bweet,
+      createdAt: Date.now(),
+    });
+    setBweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setBweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={bweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Bweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
