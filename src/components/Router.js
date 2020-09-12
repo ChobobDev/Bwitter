e555201e30d7ce0bd5acd4e64ff57f6a29c89a34
chobobdev <chobobdev@naver.com>
2020-09-12 22:19:47 +0900

    #2.6 Log Out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,9 @@
 import React from "react";
-import {HashRouter as Router, Route,Switch} from "react-router-dom";
+import {HashRouter as Router, Route,Switch, Redirect} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn }) => {
     //React Hook를 사용하는 부분 & default 값으로 false 로 둔다
@@ -9,18 +11,25 @@ const AppRouter = ({ isLoggedIn }) => {
     // #2.0 Using Firebase Auth , state(상태) 를 App.js 로 이전 
     return(
         <Router>
+            {isLoggedIn && <Navigation/>}
             <Switch>
                 {isLoggedIn ? (
                     // <> ... </> 는 fragment로 다수의 element를 route 할때 사용 할 수 있다.
                 <>
-                <Route>
+                <Route exact path="/">
                     <Home/>
+                </Route>
+                <Route exact path="/profile">
+                    <Profile/>
                 </Route>
                 </>
                 ) : (
-                <Route>
-                    <Auth/>
-                </Route>
+                <>
+                    <Route exact path="/">
+                        <Auth/>
+                    </Route>
+                    <Redirect from="*" to="/"/>
+                </>
                 )}
             </Switch>
         </Router>
