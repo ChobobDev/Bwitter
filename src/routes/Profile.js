ef5f35677a5194d5fdf3635eecd43c7a737edaad
chobobdev <chobobdev@naver.com>
2020-09-14 21:56:48 +0900

    #5.0 Get My Own Nweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,28 @@
-import React from "react";
-import { authService } from "huobase";
+import React, { useEffect } from "react";
+import { authService, dbService } from "huobase";
 import { useHistory } from "react-router-dom";
 
-export default () => {
+
+export default ({ userObj }) => {
     const history = useHistory();
     const onLogOutClick = () => {
-        authService.signOut();
-        history.push("/");
+      authService.signOut();
+      history.push("/");
     };
+
+    const getMyBweets = async () => {
+        const bweets = await dbService
+          .collection("bweets")
+          .where("creatorId", "==", userObj.uid)
+          .orderBy("createdAt")
+          .get();
+        console.log(bweets.docs.map((doc) => doc.data()));
+      };
+    
+      useEffect(() => {
+        getMyBweets();
+      }, []);
+
     return (
     <>
         <button onClick={onLogOutClick}>Log Out</button>
