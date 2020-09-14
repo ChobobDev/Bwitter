fdf5d7fba77479ca82676a5ba6072c65e2f89d30
chobobdev <chobobdev@naver.com>
2020-09-14 22:08:21 +0900

    #5.1 Update Profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,30 +1,40 @@
-import React, { useEffect } from "react";
-import { authService, dbService } from "huobase";
+import React, { useState } from "react";
+import { authService } from "huobase";
 import { useHistory } from "react-router-dom";
 
-
 export default ({ userObj }) => {
-    const history = useHistory();
-    const onLogOutClick = () => {
-      authService.signOut();
-      history.push("/");
-    };
-
-    const getMyBweets = async () => {
-        const bweets = await dbService
-          .collection("bweets")
-          .where("creatorId", "==", userObj.uid)
-          .orderBy("createdAt")
-          .get();
-        console.log(bweets.docs.map((doc) => doc.data()));
-      };
-    
-      useEffect(() => {
-        getMyBweets();
-      }, []);
-
-    return (
+  const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
+  return (
     <>
-        <button onClick={onLogOutClick}>Log Out</button>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          type="text"
+          placeholder="Display name"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
+      <button onClick={onLogOutClick}>Log Out</button>
     </>
-    )};
+  );
+};
